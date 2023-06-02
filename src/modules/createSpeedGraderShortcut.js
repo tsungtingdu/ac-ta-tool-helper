68ac3d44e400405ed07277aa26d95d63a72e3875
TomatoSoup <jonathantang0126@gmail.com>
2023-06-02 16:57:11 +0800

    fix: 🐛 avoid other change event trigger and adjust insert point

--- src/modules/createSpeedGraderShortcut.js
+++ src/modules/createSpeedGraderShortcut.js
@@ -8,17 +8,19 @@ export default () => {
   isCreateRankShortcutCalled = true
 
   const body = document.querySelector('body')
-  const insertElement = document.querySelector('.ic-Action-header__Secondary')
+  const insertElement = document.querySelector('#speed_grader_comment_textarea_mount_point')
 
-  insertShortcutSelector(insertElement, 0)
+  insertShortcutSelector(insertElement, 'canvas-rank-shortcut')
 
   body.addEventListener('change', e => {
-    const { target: { value } } = e
-    const rank = Object.keys(RANKS).find(rank => RANKS[rank].value === Number(value))
+    const { target: { value, id } } = e
+    if (id === 'canvas-rank-shortcut' && value) {
+      const rank = Object.keys(RANKS).find(rank => RANKS[rank].value === Number(value))
 
-    if (rank) {
-      const { name } = RANKS[rank]
-      postMessage(name, getStudentName())
+      if (rank) {
+        const { name } = RANKS[rank]
+        postMessage(name, getStudentName())
+      }
     }
   })
 }
@@ -29,14 +31,14 @@ function insertShortcutSelector (appendDom, id) {
   select.innerHTML = `
     <option value="">-- Quick insert --</option>
   `
-  // avoid canvas common css
-  select.style.marginBottom = '0px'
+  select.style.marginTop = '10px'
+  select.style.width = '100%'
 
   Object.keys(RANKS).forEach(rank => {
     const { name, value } = RANKS[rank]
     select.innerHTML += `<option value="${value}">${name}</option>`
   })
-  appendDom.prepend(select)
+  appendDom.append(select)
 }
 
 function postMessage (message, studentName) {
